022615f3ed5c52cefb20166b71095ccfea13f131
D4rkTab <d4rktab@yandex.ru>
2023-07-28 23:17:19 +0300

    level4 complete

--- firstApp/src/main/java/com/codemika/firstApp/controllers/UserController.java
+++ firstApp/src/main/java/com/codemika/firstApp/controllers/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/first-api")
 public class UserController {
 //    Routes:
 //    Get: /api/user @RequestParam Long id => User,
